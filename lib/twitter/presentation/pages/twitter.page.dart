c82ab1edab4e42d3eaed4bcb751d46a2f4ab8642
ugocode <obiohaugo5@gmail.com>
2023-08-10 13:09:12 +0100

    modified the twitter page

--- lib/twitter/presentation/pages/twitter.page.dart
+++ lib/twitter/presentation/pages/twitter.page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:second_portfolio/styles/personal_portfolio_icons.dart';
 
 class TwitterPage extends StatelessWidget {
   static const String route = "/twitter";
@@ -6,11 +7,40 @@ class TwitterPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text(
-        'Twitter Page!',
-        style: TextStyle(color: Colors.white),
-      ),
-    );
+    return Center(
+        child: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        const Icon(
+          PersonalPortfolioIcons.twitter,
+          size: 80,
+          color: Colors.white,
+        ),
+        const Text(
+          'Follow Me',
+          style: TextStyle(
+              fontSize: 100, fontWeight: FontWeight.bold, color: Colors.white),
+        ),
+        const Text(
+          'on Twitter',
+          style: TextStyle(
+              fontSize: 40, fontWeight: FontWeight.bold, color: Colors.white54),
+        ),
+        Container(
+          width: 300,
+          decoration: BoxDecoration(
+              color: Colors.lightBlue, borderRadius: BorderRadius.circular(50)),
+          child: const Center(
+            child: Text(
+              '@CodeHacka',
+              style: TextStyle(
+                  fontSize: 40,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white54),
+            ),
+          ),
+        ),
+      ],
+    ));
   }
 }
