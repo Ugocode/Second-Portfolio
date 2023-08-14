d152bd5202444e0afb6c05fb77e5bab3056180db
ugocode <obiohaugo5@gmail.com>
2023-08-14 08:49:28 +0100

    modified the new page

--- lib/features/website/presentation/pages/web.page.dart
+++ lib/features/website/presentation/pages/web.page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:second_portfolio/styles/personal_portfolio_icons.dart';
 
 class WebPage extends StatelessWidget {
   static const String route = "/web";
@@ -6,8 +7,40 @@ class WebPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Web Page!'),
-    );
+    return Center(
+        child: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        const Icon(
+          PersonalPortfolioIcons.web,
+          size: 80,
+          color: Colors.white,
+        ),
+        const Text(
+          "Visit",
+          style: TextStyle(
+              fontSize: 100, fontWeight: FontWeight.bold, color: Colors.white),
+        ),
+        const Text(
+          'My Page',
+          style: TextStyle(
+              fontSize: 100, fontWeight: FontWeight.bold, color: Colors.white),
+        ),
+        Container(
+          width: 300,
+          decoration: BoxDecoration(
+              color: Colors.lightBlue, borderRadius: BorderRadius.circular(50)),
+          child: const Center(
+            child: Text(
+              'www.ugocode.com',
+              style: TextStyle(
+                  fontSize: 40,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white54),
+            ),
+          ),
+        ),
+      ],
+    ));
   }
 }
