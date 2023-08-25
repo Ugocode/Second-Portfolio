7c3825df7377116144bf4bc2f69ae388ba466f12
ugocode <obiohaugo5@gmail.com>
2023-08-25 18:13:41 +0100

    created a model for linkedin

---
+++ lib/features/data/models/linkedin_page_model.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class LindkedInPageModel {
+  final String title;
+  final String url;
+  final IconData linkedInIcon;
+  final String subTitle;
+
+  LindkedInPageModel(
+      {required this.title,
+      required this.url,
+      required this.linkedInIcon,
+      required this.subTitle});
+}
