ef18871cee5b17859b4c4f13e6924d8f13877ff7
ugocode <obiohaugo5@gmail.com>
2023-09-08 09:54:47 +0100

    added provider

--- lib/features/presentation/providers/welcome_page.providers.dart
+++ lib/features/presentation/providers/welcome_page.providers.dart
@@ -1,6 +1,7 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:second_portfolio/features/data/repositories/iwelcome.repository.dart';
 import 'package:second_portfolio/features/data/repositories/mockwelcome.repository.dart';
+import 'package:second_portfolio/features/presentation/viewmodels/greetings_label.viewmodel.dart';
 
 import 'package:second_portfolio/features/presentation/viewmodels/welcome.viewmodel.dart';
 
@@ -19,7 +20,14 @@ final welcomeProvider = FutureProvider((ref) {
   return welcomeVM.getWelcomePageData();
 });
 
-// provider for the greetings to appear on rhe home screen
+// provider for the greetings to appear on the home screen
 final greetingsRawListProvider = StateProvider<List<String>>((ref) {
   return [];
+});
+
+//this displays the greetings as they changes
+final greetingsViewModelProvider =
+    StateNotifierProvider<GreetingsViewModel, String>((ref) {
+  var greetings = ref.watch(greetingsRawListProvider);
+  return GreetingsViewModel(greetings.first, greetings);
 });
